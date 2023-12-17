c05b0b649781756869dbae2f9459d4633663751d
Nerscy <146065694+Nerscy@users.noreply.github.com>
2023-12-17 17:12:13 +0100

    Chatgpt casi añadido
    casiiiii

--- Entregas/app/app.py
+++ Entregas/app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from openai import OpenAI
 import mysql.connector
-import langchain
+import funciones
+from pymysql import Error
+import os
 
 app = Flask(__name__)
 app.secret_key = 'awagga'
+key = "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"
 #---------------------------------------INDEX-------------------------------------------
 @app.route('/', methods=['GET'])
 def index():
@@ -51,67 +53,31 @@ def comando():
         return "No se ha seleccionado ningún archivo."
     else: 
         session['prompt'] = prompt
-        return redirect(url_for('resumen'))
+        return redirect(url_for('consulta'))
+
+
+#---------------------------------------------Conexion CHATGPT-------------------------------------------------
+
+@app.route('/consulta', methods=['GET'])
+def consulta():
+
+    registro = funciones.chat_gpt(key, session["prompt"], session["file"])
+    fecha = funciones.obtener_fecha_hora_actual()
+    session["registro"] = registro
+    session["fecha"] = fecha
+    return redirect(url_for('resumen'))
 
 #------------------------------------------------RESUMEN-------------------------------------------------------
     
 @app.route('/resumen', methods=['GET'])
 def resumen():
 
-    prompt = session['prompt']
-    txt = session['file']
+    registro = session["registro"]
+    return render_template('template3.html',fecha=session["fecha"], prompt=session["prompt"], respuesta=registro)
 
-    return render_template('template3.html', prompt=prompt, txt=txt)
 
 
 #----------------------------------------------CONEXION AWS----------------------------------------------------
-
-@app.route('/consulta', methods=['GET'])
-def consulta():
-    
-
-    config = {
-    'user': 'tu_usuario',
-    'password': 'tu_contraseña',
-    'host': 'endpoint_de_tu_base_de_datos_en_aws',
-    'database': 'tu_base_de_datos',
-    'port': '3306',  # Puerto por defecto para MySQL
-    }
-
-    try:
-        conexion = mysql.connector.connect(**config)
-        cursor = conexion.cursor()
-
-        cursor.execute("SELECT * FROM tu_tabla")
-        resultados = cursor.fetchall()
-
-        """for resultado in resultados:
-            print(resultado)"""
-        
-    except mysql.connector.Error as err:
-        print(f"Error de conexión: {err}")
-
-    finally:
-        # Asegurarse de cerrar la conexión, independientemente de si fue exitosa o no
-        if 'conexion' in locals() and conexion.is_connected():
-            cursor.close()
-            conexion.close()
-            print("Conexión cerrada.")  
-
-    #------------------------------------------- CONSULTA CHATGPT----------------------------------
-    try:
-        client = OpenAI(api_key="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")
-        completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "For the moment this is a test."},
-            {"role": "user", "content": "Please, confirm me by saying anything."}
-        ]
-        )
-
-        return completion.choices[0].message.content
-    except:
-        return "ha ocurrido un error con chatgpt"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
