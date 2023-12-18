d866106fa3e5978ac0703774a8f1b255a3b0a1d4
dardg24 <danielrdegouveia@gmail.com>
2023-12-18 22:04:06 +0000

    "readme y ordenado"

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# chatbot_gpt
-We're developing an application using the openAI API and storing a database in AWS. 
+
+# Proyecto Flask con Docker
+
+Este proyecto consiste en una aplicación web desarrollada con Flask y desplegada en un contenedor Docker.
+
+## Autores
+
+- Ana Fernandez
+- Adrián Nieto
+- Daniel Gouveia
+- Daniel Manso
+- Guillermo Pereda
+
+## Características
+
+- Aplicación Flask para responder preguntas utilizando GPT.
+- Uso de Docker para contenerizar y desplegar la aplicación.
+- Conexión a base de datos MySQL para almacenamiento de datos.
+
+## Uso
+
+Para ejecutar la aplicación en un contenedor Docker, sigue estos pasos:
+
+1. Clona el repositorio:
+   ```bash
+   git clone [URL del repositorio]
+   ```
+
+2. Construye la imagen Docker:
+   ```bash
+   docker build -t mi-app-flask .
+   ```
+
+3. Ejecuta la imagen en un contenedor:
+   - Asegúrate de que el puerto que deseas usar esté libre.
+   - Utiliza el comando `docker run` con el mapeo de puertos apropiado. Por ejemplo:
+     ```bash
+     docker run -p 5000:5000 mi-app-flask
+     ```
+
+4. Accede a la aplicación a través de `http://localhost:5000` en tu navegador.
+
+## Despliegue en Docker
+
+El `Dockerfile` incluido en este repositorio se utiliza para construir la imagen de Docker de la aplicación. La configuración es la siguiente:
+
+- Imagen base de Python 3.9-slim.
+- Instalación de dependencias desde `requirements.txt`.
+- Exposición del puerto 5000.
+- Uso de Gunicorn como servidor web.
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Para más detalles, ver el archivo `LICENSE`.
+
+---
+
+*Este README fue generado automáticamente con la asistencia de ChatGPT.*
---
+++ app/funciones.py
@@ -0,0 +1,78 @@
+#from dotenv import load_dotenv
+import os
+from langchain.chains import AnalyzeDocumentChain
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from datetime import datetime
+
+
+
+def chat_gpt(api_key,question, file):
+
+    llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=api_key)
+
+    qa_chain = load_qa_chain(llm, chain_type="map_reduce")
+    print(qa_chain)
+    qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=qa_chain)
+    print(qa_document_chain)
+    """with open("./pregunta.txt") as f:
+        file = f.read()"""
+    print((type(file)))
+
+    # Obtener la fecha y hora actuales
+    fecha_hora_actual = datetime.now()
+    fecha_hora_formateada = fecha_hora_actual.strftime('%Y-%m-%d %H:%M:%S')
+
+    
+    response = qa_document_chain.run(
+        input_document=file,
+        question=question,
+    )
+    
+
+    print(response)
+
+    # Crear un registro con la pregunta, respuesta y fecha y hora
+    registro = {
+        "pregunta": question,
+        "respuesta": response,
+        "fecha_hora": fecha_hora_formateada
+    }
+    # Imprimir el registro
+    return registro
+
+
+
+   
+
+"""def leer_archivo(ruta_archivo):
+    try:
+        with open(ruta_archivo) as f:
+            return f.read()
+    except Exception as e:
+        print(f"Error al leer el archivo: {e}")
+        return None"""
+
+def obtener_fecha_hora_actual():
+    try:
+        fecha_hora_actual = datetime.now()
+        return fecha_hora_actual.strftime('%Y-%m-%d %H:%M:%S')
+    except Exception as e:
+        print(f"Error al obtener la fecha y hora: {e}")
+        return fecha_hora_actual
+
+def procesar_documento(api_key, ruta_archivo, question):
+    try:
+        llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=api_key)
+        qa_chain = load_qa_chain(llm, chain_type="map_reduce")
+        qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=qa_chain)
+
+        documento = leer_archivo(ruta_archivo)
+        if documento is None:
+            return None
+
+        response = qa_document_chain.run(input_document=documento, question=question)
+        return response
+    except Exception as e:
+        print(f"Error al procesar el documento: {e}")
+        return None
